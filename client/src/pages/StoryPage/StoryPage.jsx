1a9d3baf733c7085e93d5d79e356dee8dba4822c
graciaanna032203 <graceannegarchitorena@gmail.com>
2024-12-14 22:36:44 +0800

    feat: added paper mode

--- client/src/pages/StoryPage/StoryPage.jsx
+++ client/src/pages/StoryPage/StoryPage.jsx
@@ -5,12 +5,16 @@ import InsideNavbar from '../../components/Navbar/InsideNavbar';
 import Footer from "../../components/Footer/Footer"; 
 import "./StoryPage.css";
  
-import Ball from "../../assets/logo-2.svg";
+import Paper from "../../assets/paper-mode.svg";
 import FrontCover from "../../assets/book-front-cover.png";
 import BackCover from "../../assets/book-back-cover.png";
 import BackCover1 from "../../assets/book-back-cover1.png";
 import LeftPageImg from "../../assets/book-left-page.png";
-import RightPageImg from "../../assets/book-right-page.png";
+import RightPageImg from "../../assets/book-right-page.png"; 
+
+import { TiDocumentText } from "react-icons/ti"; 
+import { IoMdDownload } from "react-icons/io"; 
+import { VscBook } from "react-icons/vsc";
 
 const Page = React.forwardRef((props, ref) => (
     <div className="page" ref={ref}>
@@ -26,6 +30,7 @@ const StoryPage = () => {
     const { title = "Generated Story", story = "", loading = false } = location.state || {};
     const [storyPages, setStoryPages] = useState([]);
     const [currentPage, setCurrentPage] = useState(0);
+    const [isPaperMode, setIsPaperMode] = useState(false); // Toggle between book and paper mode
     const bookRef = useRef();
 
     const width = window.innerWidth;
@@ -35,9 +40,11 @@ const StoryPage = () => {
     useEffect(() => {
         if (loading) return;
 
+        // Clean the title and story to remove content before and including [SEP]
         const cleanTitle = title.includes("[SEP]") ? title.split("[SEP]")[1].trim() : title;
         const cleanStory = story.includes("[SEP]") ? story.split("[SEP]")[1].trim() : story;
 
+        // Split the story into pages for the flipbook mode
         const words = cleanStory.split(" ");
         const wordsPerPage = 50;
         const pages = [];
@@ -53,7 +60,7 @@ const StoryPage = () => {
         setCurrentPage(e.data);
 
         const progressBar = document.querySelector(".progress-bar");
-        const totalPages = storyPages.length + 4;
+        const totalPages = storyPages.length + 4; // Including covers
         const progressValue = e.data / (totalPages - 1);
 
         const progressWidth = progressValue === 0 ? 0 : Math.max(3, Math.min(97, progressValue * 100));
@@ -61,7 +68,7 @@ const StoryPage = () => {
     };
 
     const handleProgressBarChange = (event) => {
-        const totalPages = storyPages.length + 4;
+        const totalPages = storyPages.length + 4; // Including covers
         const progressValue = parseFloat(event.target.value) / 100;
 
         const progressValueAdjusted = Math.max(0.10, Math.min(0.90, progressValue));
@@ -86,37 +93,50 @@ const StoryPage = () => {
                 <section className="story-page-container">
                     {!loading && (
                         <>
-                            <HTMLFlipBook
-                                width={flipbookWidth}
-                                height={flipbookHeight}
-                                maxShadowOpacity={0.2}
-                                showCover={true}
-                                ref={bookRef}
-                                onFlip={handlePageChange}
-                            >
-                                <Page image={FrontCover} />
-                                <Page image={BackCover} />
-                                <Page
-                                    image={RightPageImg}
-                                    content={
-                                        <>
-                                            <p className="story-subtitle">Ang tipo kong Kwento</p>
-                                            <h1 className="story-title">"{title.includes("[SEP]") ? title.split("[SEP]")[1].trim() : title}"</h1>
-                                        </>
-                                    }
-                                />
-                                <Page image={LeftPageImg} content={storyPages[0]} />
-                                {storyPages.slice(1).map((content, index) => (
+                            {!isPaperMode && (
+                                <HTMLFlipBook
+                                    width={flipbookWidth}
+                                    height={flipbookHeight}
+                                    maxShadowOpacity={0.2}
+                                    showCover={true}
+                                    ref={bookRef}
+                                    onFlip={handlePageChange}
+                                >
+                                    <Page image={FrontCover} />
+                                    <Page image={BackCover} />
                                     <Page
-                                        key={index}
-                                        image={index % 2 === 0 ? RightPageImg : LeftPageImg}
-                                        content={content}
+                                        image={RightPageImg}
+                                        content={
+                                            <>
+                                                <p className="story-subtitle">Ang tipo kong Kwento</p>
+                                                <h1 className="story-title">"{title.includes("[SEP]") ? title.split("[SEP]")[1].trim() : title}"</h1>
+                                            </>
+                                        }
                                     />
-                                ))}
-                                <Page image={BackCover1} />
-                                <Page image={BackCover} />
-                            </HTMLFlipBook>
-
+                                    <Page image={LeftPageImg} content={storyPages[0]} />
+                                    {storyPages.slice(1).map((content, index) => (
+                                        <Page
+                                            key={index}
+                                            image={index % 2 === 0 ? RightPageImg : LeftPageImg}
+                                            content={content}
+                                        />
+                                    ))}
+                                    <Page image={BackCover1} />
+                                    <Page image={BackCover} />
+                                </HTMLFlipBook>
+                            )}
+                            {isPaperMode && (
+                                <div className="paper-mode">
+                                    <img src={Paper} alt="Paper Background" className="paper-image" />
+                                    <div className="paper-content">
+                                        <p className="story-subtitle">Ang tipo kong Kwento</p>
+                                        <h1 className="story-title">"{title.includes("[SEP]") ? title.split("[SEP]")[1].trim() : title}"</h1>
+                                        <p className="story-text">
+                                            {story.includes("[SEP]") ? story.split("[SEP]")[1].trim() : story}
+                                        </p>
+                                    </div>
+                                </div>
+                            )}
                             <div className="progress-bar-container">
                                 <input
                                     type="range"
@@ -128,6 +148,13 @@ const StoryPage = () => {
                                     onInput={handleProgressBarChange}
                                     onChange={handleProgressBarChange}
                                 />
+                                <div className="paper-icon" onClick={() => setIsPaperMode(!isPaperMode)}>
+                                    {isPaperMode ? <VscBook /> : <TiDocumentText />}
+                                </div>
+
+                                <div className="download-btn">
+                                    <IoMdDownload /> 
+                                </div>
                             </div>
                         </>
                     )}
@@ -139,4 +166,3 @@ const StoryPage = () => {
 };
 
 export default StoryPage;
-
